ffc63a70f27efcb5d7f9934c20b4e6bc21fd43a1
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2023-03-29 20:00:30 +0300

    working quickSort solution

--- src/Main.java
+++ src/Main.java
@@ -6,39 +6,95 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int[] input = Arrays.stream(bf.readLine().split(" ")).mapToInt(x -> Integer.parseInt(x)).toArray();
-        int a = input[0];
-        int c = input[1];
-        int m = input[2];
-        int seed = input[3];
+        long[] input = Arrays.stream(bf.readLine().split(" ")).mapToLong(x -> Long.parseLong(x)).toArray();
+        long a = input[0];
+        long c = input[1];
+        long m = input[2];
+        long seed = input[3];
         int n = Integer.parseInt(bf.readLine());
 
-        double[] nums = generateRandomNumbers(a, c, m, seed, n);
+        Double[] nums = generateRandomNumbers(a, c, m, seed, n);
         double median = getMedian(nums);
         int index = getIndex(nums, median);
 
-        System.out.println(index);
+        System.out.println(index + 1);
     }
 
-    public static double[] generateRandomNumbers(int a, int c, int m, int seed, int n){
-        // TODO
-        return new double[]{0d};
+    public static Double[] generateRandomNumbers(long a, long c, long m, long seed, int n) {
+        Long[] arrX = new Long[n + 1];
+        Double[] arrY = new Double[n];
+        arrX[0] = seed;
+        for (int i = 1; i < n + 1; i++) {
+            long x =  mod((a * arrX[i - 1] + c), m);
+            arrX[i] = x;
+            Double y = Math.abs((2 * (double)x / m) - 1);
+            arrY[i - 1] = y;
+        }
+        return arrY;
     }
 
-    public static double getMedian(double[] arr){
-        // TODO
-        return 0d;
+    private static long mod(long x, long y)
+    {
+        long result = x % y;
+        while (result < 0)
+        {
+            result += y;
+        }
+        return result;
     }
 
-    public static int getIndex(double[] arr, double elem){
-        // TODO
+    public static double getMedian(Double[] arr) {
+        Double[] tempArray = arr.clone();
+        Sort<Double> sort = new Sort<>();
+        sort.quickSort(tempArray, 0, arr.length - 1);
+
+        int index = tempArray.length / 2 - 1;
+        return tempArray[index];
+    }
+
+    public static int getIndex(Double[] arr, Double elem) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(elem)) {
+                return i;
+            }
+        }
         return -1;
     }
 
 }
 
-class Sort<T extends Comparable<T>>{
-    public void bucketSort(T[] array){
+class Sort<T extends Comparable<T>> {
+    public void bucketSort(T[] array) {
         // TODO
+    }
+
+    public void quickSort(T[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private int partition(T[] arr, int begin, int end) {
+        T pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j].compareTo(pivot) < 0) {
+                i++;
+
+                T swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        T swapTemp = arr[i + 1];
+        arr[i + 1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i + 1;
     }
 }
